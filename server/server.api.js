48ff05adfd07a8a5c22b708781473cd218216595
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2017-03-30 17:18:44 -0400

    Added logic to sort trends data by traffic count.

--- server/server.api.js
+++ server/server.api.js
@@ -5,8 +5,17 @@
 
 var googleTrends = require("google-trends-api-es");
 
+function approxTrafficToInteger(item) {
+	return parseInt(item.slice(0, item.length - 1).replace(/\,/g,'') ,10);
+}
+
+function sortByTraffic(a, b) {
+	return approxTrafficToInteger(b["ht:approx_traffic"][0]) - approxTrafficToInteger(a["ht:approx_traffic"][0])
+}
+
 function getTrends(request, response) {
 	googleTrends.hotTrendsDetail("US").then(function(results) {
+		results.rss.channel[0].item.sort(sortByTraffic);
 		response.json(results);
 	}).catch(function(err){
 		response.json({error: err});
